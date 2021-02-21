b859267603dc02a4b4a61c086a55eea967c67405
WejdanCS <wejdanaljadani20@gmail.com>
2021-02-21 03:16:07 +0300

    feat:Create & get weather city and pics information from WratherBit & PixaBay APIs

--- src/server/APIs.js
+++ src/server/APIs.js
@@ -2,17 +2,22 @@ const dotenv = require("dotenv");
 dotenv.config();
 const fetch = require("node-fetch");
 // var city = "london"
-const userName = process.env.GEO_USER_NAME;
+const geoNameApiUserName = process.env.GEO_USER_NAME;
+const weatherBitAPIKey = process.env.WEATHER_BIT_API_KEY;
+const pixaBayAPIKey = process.env.PIXA_BAY_API_KEY;
 
 // Define global variables
-const GeoNamesBase_Url = 'http://api.geonames.org/';
+const geoNamesBase_Url = 'http://api.geonames.org/';
+const weatherBitAPI_Url = 'https://api.weatherbit.io/v2.0/';
+const pixaBayAPI_Url = 'https://pixabay.com/api/';
+
 
 var getCityInfo = async(city) => {
     // console.log(city)
-    var GeoNamesAPI_Url = `${GeoNamesBase_Url}searchJSON?&q=${city}&username=${userName}`;
+    var geoNamesAPI_Url = `${geoNamesBase_Url}searchJSON?&q=${city}&username=${geoNameApiUserName}`;
 
     try {
-        var response = await fetch(GeoNamesAPI_Url);
+        var response = await fetch(geoNamesAPI_Url);
         var cityDetails = await response.json();
         var cityInfo = cityDetails.geonames[0];
         return {
@@ -26,9 +31,62 @@ var getCityInfo = async(city) => {
     }
 }
 
+var getWeatherStatus = async(cityInfo, date) => {
+    // https://api.weatherbit.io/v2.0/current
+    var weatherApiUrl = `${weatherBitAPI_Url}/current?lat=${cityInfo.latitude}&lon=${cityInfo.longitude}&key=${weatherBitAPIKey}`;
+    try {
+        var response = await fetch(weatherApiUrl);
+        var weatherDetails = await response.json();
+        // console.log(weatherDetails.data)
+        var weatherStatus = weatherDetails.data[0];
+        return {
+            windDir: weatherStatus.wind_cdir_full,
+            temp: weatherStatus.temp,
+            weather: weatherStatus.weather.description
+
+
+        };
+
+    } catch (error) {
+        console.log(`ERROR:${error}`);
+    }
+
+}
+var getCityPic = async(city) => {
+
+    var picRequestUrl = `${pixaBayAPI_Url}/?key=${pixaBayAPIKey}&q=${city}&image_type=photo&category=places&per_page=8`;
+
+    try {
+        var response = await fetch(picRequestUrl);
+        var allPics = await response.json();
+        // console.log(allPics)
+        var selectedPics = allPics.hits;
+        picsUrl = [];
+
+        selectedPics.forEach(pic => {
+            picsUrl.push(pic.pageURL);
+            console.log(pic.pageURL)
+
+        });
+        return {
+
+            picsUrl
+
+
+        };
+
+    } catch (error) {
+        console.log(`ERROR:${error}`);
+    }
+
+
+}
 
 
 
 module.exports = {
-    getCityInfo
+    getCityInfo,
+    getWeatherStatus,
+    getCityPic
+
 }
--- src/server/server.js
+++ src/server/server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const cors = require("cors");
 const bodyParser = require("body-parser")
 const app = express()
-const { getCityInfo } = require('./APIs');
+const { getCityInfo, getWeatherStatus, getCityPic } = require('./APIs');
 app.use(cors())
 app.use(bodyParser.json())
     // to use url encoded values
@@ -18,9 +18,18 @@ app.listen(8081, function() {
 });
 
 app.post("/postCityInfo", async(req, res) => {
+    const city = req.body.city;
+
 
     // console.log(req.body.city);
-    var cityInfo = await getCityInfo(req.body.city);
-    res.send(cityInfo)
+    var cityInfo = await getCityInfo(city);
+    // res.send(cityInfo)
+
+    // vaer 
+    var weatherInfo = await getWeatherStatus(cityInfo, "20-02-2021");
+    console.log(weatherInfo)
+    var weatherInfo = await getCityPic(city);
+
+    res.send(weatherInfo)
 
 });
